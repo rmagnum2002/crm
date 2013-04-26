df765ed91cadd6504d52db5d815e772478a79121
Rosca Sergiu <rmagnum2002@gmail.com>
2013-04-26 12:30:30 +0300

    event calendar part 1

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,7 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+    @events_by_date = @events.group_by(&:event_date)
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+  end
+end

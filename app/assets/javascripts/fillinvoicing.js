7a27b0643e55978b8d4eab569a3a7ade05bb892e
Alex Leasenco <alexwebscom@gmail.com>
2013-05-01 13:19:30 +0300

    fixed fillinvoicing.js

--- app/assets/javascripts/fillinvoicing.js
+++ app/assets/javascripts/fillinvoicing.js
@@ -1,28 +1,51 @@
-  function FillInvoicing() {
+$(document).ready(function(){
 
-    if(fill_invoicing.checked == true) {
+if($("#company_addresses_attributes_1_country_id").val().length > 0) {
 
-      $("#company_addresses_attributes_1_country_id").val( $("#company_addresses_attributes_0_country_id").val());
+    $(".controls.state_0").css("display", "");
+    $(".controls.state_1").css("display", "");
 
-      if($("#company_addresses_attributes_1_country_id").val().length > 0) {
+    $(".controls.state_0 option[value='']").each(function() {
+      $(this).remove();
+    });
 
-        $("#company_addresses_attributes_1_state_id").val($("#company_addresses_attributes_0_state_id").val());
+    $(".controls.state_1 option[value='']").each(function() {
+      $(this).remove();
+    });
 
-        $(".controls.state_1").css("display", "");
+    $(".controls.state_0 optgroup").toggle();
 
-        $(".controls.state_1 option[value='']").each(function() {
-          $(this).remove();
-        });
+    $(".controls.state_1 optgroup").toggle();
 
-        $(".controls.state_1 optgroup").toggle();
+}
 
-      }
+})
 
-      $("#company_addresses_attributes_1_postal_code").val( $("#company_addresses_attributes_0_postal_code").val() );
+function FillInvoicing() {
 
-      $("#company_addresses_attributes_1_street").val( $("#company_addresses_attributes_0_street").val() );
+  if(fill_invoicing.checked == true) {
 
-      $("#company_addresses_attributes_1_street_number").val( $("#company_addresses_attributes_0_street_number").val() );
+    $("#company_addresses_attributes_1_country_id").val( $("#company_addresses_attributes_0_country_id").val());
+
+    if($("#company_addresses_attributes_1_country_id").val().length > 0) {
+
+      $("#company_addresses_attributes_1_state_id").val($("#company_addresses_attributes_0_state_id").val());
+
+      $(".controls.state_1").css("display", "");
+
+      $(".controls.state_1 option[value='']").each(function() {
+        $(this).remove();
+      });
+
+      $(".controls.state_1 optgroup").toggle();
+
+    }
+
+    $("#company_addresses_attributes_1_postal_code").val( $("#company_addresses_attributes_0_postal_code").val() );
+
+    $("#company_addresses_attributes_1_street").val( $("#company_addresses_attributes_0_street").val() );
+
+    $("#company_addresses_attributes_1_street_number").val( $("#company_addresses_attributes_0_street_number").val() );
   }
 
 }

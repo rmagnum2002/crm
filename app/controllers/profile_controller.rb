20665cc698c2f5db6cdeda2c8fb57e1652167058
Rosca Sergiu <rmagnum2002@gmail.com>
2013-05-02 16:29:02 +0300

    added events to user profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,6 +6,7 @@ class ProfileController < ApplicationController
   def show
     @user = User.find(params[:id])
     @responsible = Company.where(responsible_id: @user.id)
+    @activities = @user.activities.order("created_at desc")
 
     respond_to do |format|
       format.html # show.html.erb

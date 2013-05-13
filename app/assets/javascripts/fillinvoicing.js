aef53a748e7f834afadc674fe3709d4e17f8af41
alexwebs <alexwebscom@gmail.com>
2013-05-13 23:00:01 +0300

    fixed fillinvoicing in edit company

--- app/assets/javascripts/fillinvoicing.js
+++ app/assets/javascripts/fillinvoicing.js
@@ -1,7 +1,7 @@
 function fill_form() {
   var select = $("select#state_id_0").val();
   $("#company_addresses_attributes_1_country_id").val($("#company_addresses_attributes_0_country_id").val());
-  $('select#state_id_1').html($('select#state_id_0').html())
+  $('select#state_id_1').html($('select#state_id_0').html()).attr('class', 'state_id_1');
   $('select#state_id_1').val(select);
   $("#company_addresses_attributes_1_postal_code").val( $("#company_addresses_attributes_0_postal_code").val() );
   $("#company_addresses_attributes_1_street").val( $("#company_addresses_attributes_0_street").val() );

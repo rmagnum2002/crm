a362a0a74aeb3c59579c12c923714e1840e95637
alexwebs <alexwebscom@gmail.com>
2013-05-14 17:23:08 +0300

    fixed fillinvoicing in new company

--- app/assets/javascripts/fillinvoicing.js
+++ app/assets/javascripts/fillinvoicing.js
@@ -1,7 +1,12 @@
 function fill_form() {
-  var select = $("select#state_id_0").val();
-  $("#company_addresses_attributes_1_country_id").val($("#company_addresses_attributes_0_country_id").val());
-  $('select#state_id_1').html($('select#state_id_0').html()).attr('class', 'state_id_1');
+	var select = $("select#state_id_0").val();
+	$("#company_addresses_attributes_1_country_id").val($("#company_addresses_attributes_0_country_id").val());
+		if ($('#state_id_1').length == 1) {
+			$('select#state_id_1').html($('select#state_id_0').html()).attr('name', 'company[addresses_attributes][1][state_id]');
+		}
+		else {
+			$("#state_id_0").clone().appendTo($('.controls.state_1')).attr('id', 'state_id_1').attr('name', 'company[addresses_attributes][1][state_id]');
+		}
   $('select#state_id_1').val(select);
   $("#company_addresses_attributes_1_postal_code").val( $("#company_addresses_attributes_0_postal_code").val() );
   $("#company_addresses_attributes_1_street").val( $("#company_addresses_attributes_0_street").val() );

6a855b3a469dbe5f7e357d37709627eab996f210
Rosca Sergiu <rmagnum2002@gmail.com>
2013-05-15 13:32:11 +0300

    ajax pagination in profile user page for activities

--- app/assets/javascripts/app.js.coffee
+++ app/assets/javascripts/app.js.coffee
@@ -2,3 +2,8 @@ jQuery ->
   $('#company_client_at, #employee_birthday, #employee_client_date').datepicker
     dateFormat: 'yy-mm-dd'
     # monthNames: ['Ianuarie','Februarie','Martie','Aprilie','Mai','Iunie', 'Iulie','August','Septembrie','Octombrie','Noiembrie','Decembrie'],
+
+$ ->
+  $(".pagination a").on "click", ->
+    $.getScript @href
+    false
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,12 +6,13 @@ class ProfileController < ApplicationController
   def show
     @user = User.find(params[:id])
     @responsible = Company.where(responsible_id: @user.id)
-    @activities = @user.activities.order("created_at desc")
+    @activities = @user.activities.order("created_at desc").paginate(:page => params[:page], :per_page => 10)
     @total = @user.sales.sum(&:ammount)
 
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @profile }
+      format.js
     end
   end
 end

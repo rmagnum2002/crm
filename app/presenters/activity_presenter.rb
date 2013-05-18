a5b44969ae6c348333688a033459ece246101c11
Sergiu <sergiu@sergiu-MS-7758.(none)>
2013-05-18 13:31:43 +0300

    added new icons and blocks for activities

--- app/presenters/activity_presenter.rb
+++ app/presenters/activity_presenter.rb
@@ -8,14 +8,29 @@ class ActivityPresenter < SimpleDelegator
 
   def render_activity
     div_for activity do
-      content_tag(:div, class: activity.trackable_type.downcase) do
-        content_tag(:span, class: "span3") do
-          link_to(activity.user.full_name, profile_path(activity.user)) + "<br>".html_safe +
-          (l activity.created_at, format: :long)
-        end +
-        content_tag(:span, class: "span6") do
-           render_partial
+      content_tag(:td) do
+        link_to(activity.user.full_name, profile_path(activity.user)) + "<br>".html_safe +
+        (l activity.created_at, format: :long)
+      end +
+      content_tag(:td) do
+        if activity.trackable_type == "Employee"
+          "<i class='icon-user'></i>".html_safe
+        elsif activity.trackable_type == "Company"
+          if activity.action == "create"
+            "<i class='icon-plus'></i>".html_safe
+          elsif activity.action == "update"
+            "<i class='icon-edit'></i>".html_safe
+          end
+        elsif activity.trackable_type == "Event"
+          "<i class='icon-calendar'></i>".html_safe
+        elsif activity.trackable_type == "Comment"
+          "<i class='icon-comment'></i>".html_safe
+        elsif activity.trackable_type == "Sale"
+          "<i class='icon-barcode'></i>".html_safe
         end
+      end +
+      content_tag(:td) do
+         render_partial
       end
     end
   end

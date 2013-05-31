65d7c082ba77fa898e8bd81c9e2da9967f8fdb1d
Rosca Sergiu <rmagnum2002@gmail.com>
2013-05-31 11:20:25 +0300

    updated ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,13 +6,20 @@ class Ability
     #
     user ||= User.new # guest user (not logged in)
 
+    # alias_action :create, :read, :update, :destroy, :to => :crud
+    alias_action :show_contacts, :show_address, :sales, :comments, :country_select_legal, :country_select_invoicing, :to => :company_actions
+    alias_action :events_for_day, :open_details, :to => :events_actions
+
     if user.role? :admin
       can :manage, :all
-    elsif user.role? :user
-      can :read, :all
     elsif user.role? :moderator
-      can :create, Company
+      can :read, Company
       can :update, Company, :user_id => user.id
+      can :destroy, Company, :user_id => user.id
+      can :company_actions, Company
+      can :create, Company
+      can :read, Event
+      can :events_actions, Event
     end
     #
     # The first argument to `can` is the action you are giving the user

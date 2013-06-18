ab3228a75d6709ef3715b11b1558ea98944f4718
Alex Leasenco <alexwebscom@gmail.com>
2013-06-18 12:23:49 +0300

    added condition for fields in address form

--- app/assets/javascripts/fillinvoicing.js
+++ app/assets/javascripts/fillinvoicing.js
@@ -2,21 +2,29 @@ function fill_form() {
 	var select = $("select#state_id_0").val();
   if ($(".controls.country_1") != "") {
     $(".controls.country_1").empty();
-    $("#company_addresses_attributes_0_country_id").clone().appendTo($(".controls.country_1")).attr('id', 'company_addresses_attributes_1_country_id')
-      .attr('name', 'company[addresses_attributes][1][country_id]');
+    $("#company_addresses_attributes_0_country_id").clone().appendTo($(".controls.country_1"))
+    .attr('id', 'company_addresses_attributes_1_country_id')
+    .attr('name', 'company[addresses_attributes][1][country_id]');
     $("#company_addresses_attributes_1_country_id").val($("#company_addresses_attributes_0_country_id").val());
+  }
 
+  if ($('#company_addresses_attributes_0_postal_code').val() != "") {
     $(".controls.postal_code_1").empty();
-    $("#company_addresses_attributes_0_postal_code").clone().appendTo($(".controls.postal_code_1")).attr('id', 'company_addresses_attributes_1_postal_code')
-      .attr('name', 'company[addresses_attributes][1][postal_code]');
-
+    $("#company_addresses_attributes_0_postal_code").clone().appendTo($(".controls.postal_code_1"))
+    .attr('id', 'company_addresses_attributes_1_postal_code')
+    .attr('name', 'company[addresses_attributes][1][postal_code]');
+  }
+  if ($('#company_addresses_attributes_0_street').val() != "") {
     $(".controls.street_1").empty();
-    $("#company_addresses_attributes_0_street").clone().appendTo($(".controls.street_1")).attr('id', 'company_addresses_attributes_1_street')
-      .attr('name', 'company[addresses_attributes][1][street]');
-
+    $("#company_addresses_attributes_0_street").clone().appendTo($(".controls.street_1"))
+    .attr('id', 'company_addresses_attributes_1_street')
+    .attr('name', 'company[addresses_attributes][1][street]');
+  }
+  if ($('#company_addresses_attributes_0_street_number').val() != "") {
     $(".controls.street_number_1").empty();
-    $("#company_addresses_attributes_0_postal_code").clone().appendTo($(".controls.street_number_1")).attr('id', 'company_addresses_attributes_1_street_number')
-      .attr('name', 'company[addresses_attributes][1][street_number]');
+    $("#company_addresses_attributes_0_street_number").clone().appendTo($(".controls.street_number_1"))
+    .attr('id', 'company_addresses_attributes_1_street_number')
+    .attr('name', 'company[addresses_attributes][1][street_number]');
   }
   if ($('#state_id_1').length == 1) {
 		$('select#state_id_1').html($('select#state_id_0').html()).attr('name', 'company[addresses_attributes][1][state_id]');
@@ -25,9 +33,9 @@ function fill_form() {
 		$("#state_id_0").clone().appendTo($('.controls.state_1')).attr('id', 'state_id_1').attr('name', 'company[addresses_attributes][1][state_id]');
 	}
     $('select#state_id_1').val(select);
-    $("#company_addresses_attributes_1_postal_code").val( $("#company_addresses_attributes_0_postal_code").val() );
-    $("#company_addresses_attributes_1_street").val( $("#company_addresses_attributes_0_street").val() );
-    $("#company_addresses_attributes_1_street_number").val( $("#company_addresses_attributes_0_street_number").val() );
+    $("#company_addresses_attributes_1_postal_code").val($("#company_addresses_attributes_0_postal_code").val());
+    $("#company_addresses_attributes_1_street").val($("#company_addresses_attributes_0_street").val());
+    $("#company_addresses_attributes_1_street_number").val($("#company_addresses_attributes_0_street_number").val());
 }
 
 var fnShowHide;

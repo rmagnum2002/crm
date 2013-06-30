e46e346dd44f3fc563734cce4f04a08ab1c4b40a
alex <alex@ruby.(none)>
2013-06-30 17:00:14 +0300

    added rspec for create and destroy method

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -10,11 +10,21 @@ describe CommentsController do
   end
 	
 	describe "POST create" do
-		it "should create a comment" do
-			pending
+		it "should create comment" do
+			sign_in @user
+			lambda {
+      # session[:user_id] = @user.id
+      xhr :post, :create, { comment: { content: "new comment" }, company_id: @company.id } }
+      .should change(Comment, :count).by(1)
+      response.should render_template("comments/create")
 		end
-			
 	end
 
-
+	describe "DELETE comment" do
+		it "should delete comment" do
+			sign_in @user
+			xhr :delete, :destroy, { id: @comment.id }
+			response.should render_template("comments/destroy")
+		end
+	end
 end

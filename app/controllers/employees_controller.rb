9b43a3e62f9e76a3c79213dd1182ec403304336b
SergiuR <rmagnum2002@gmail.com>
2013-11-18 16:35:42 +0200

    Update employees_controller.rb

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,5 @@
 class EmployeesController < ApplicationController
+  before_filter :authenticate_user!
   before_filter :load_company, except: ['index', 'profile', 'revisions']
 
   load_and_authorize_resource
